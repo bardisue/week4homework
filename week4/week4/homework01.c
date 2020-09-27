012bc6d80a1d5b712a9b3f3457ebaf55ee295e32
tr0568@o.cnu.ac.kr <tr0568@o.cnu.ac.kr>
2020-09-27 15:25:45 +0900

    homework01 완료

---
+++ week4/week4/homework01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char s[100];
+	printf("문자를 입력하세요(100자 이내).");
+	scanf("%s", s);
+	printf("입력된 문자는 %s입니다.\n", s);
+	system("pause"); // keep terminal open
+	return 0;
+}
